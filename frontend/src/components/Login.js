67f42ecb5ab4b82586cb7b52c1a1666745a105f1
chamod445 <chamodsanjula445@gmail.com>
2022-05-23 21:07:04 +0530

    updated

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,84 +2,22 @@ import React, { useState } from "react";
 import "../styles/css/login.css";
 
 function App() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  // User Login info
-  const database = [
-    {
-      username: "admin",
-      password: "admin"
-    }
-  ];
-
-  const errors = {
-    uname: "Invalid Username",
-    pass: "Invalid Password"
-  };
-
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-        window.location.href = '/adminhome';
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
-
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-
-  // JSX code for login form
-  const renderForm = (
     <div className="form">
       <form onSubmit={handleSubmit}>
         <div className="input-container">
           <label>Username </label>
           <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
         </div>
+
         <div className="input-container">
           <label>Password </label>
           <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
         </div>
+
         <div className="button-container">
-          <input type="submit" />
+         <a href="/adminhome"><button type="button" /></a>
         </div>
+
       </form>
     </div>
-  );
-
-  return (
-    <div className="login">
-      <div className="app">
-        <div className="login-form">
-          <div className="title">Sign In</div>
-          {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-        </div>
-      </div>
-    </div>
-  );
 }
-
-export default App;

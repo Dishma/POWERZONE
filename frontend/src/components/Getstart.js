5181f98eea7a5955e1a786bde4f2be55e877ad42
chamod445 <chamodsanjula445@gmail.com>
2022-05-23 18:16:19 +0530

    Updated by Chamod

--- frontend/src/components/Getstart.js
+++ frontend/src/components/Getstart.js
@@ -15,8 +15,8 @@ function Getstart() {
         <div class="carousel-item active">
           <img src="https://res.cloudinary.com/dnonvyjrq/image/upload/v1651902201/pexels-victor-freitas-841130_p1z6r3.jpg" class="d-block w-100" ></img>
           <div class="carousel-caption d-none d-md-block">
-            <h5>“Your health account, your bank account, they’re the same thing. The more you put in, the more you can take out.”</h5>
-            <p>-Jack LaLanne</p>
+            <h5 style={{fontSize: '18.2px'}}>“Your health account, your bank account, they’re the same thing. The more you put in, the more you can take out.”</h5>
+            <p>- Jack LaLanne -</p>
             <a role="button" className="getstartbutton adminbutton2" href="/login"><span>Get Started</span></a>
             <br />
             <br />

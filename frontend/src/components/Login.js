4347fb685ece51d501bd2d4522622fd3079f2ec6
chamod445 <chamodsanjula445@gmail.com>
2022-05-24 11:08:00 +0530

    Updated

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import "../styles/css/login.css";
 
 function App() {
-  // React States
+
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  // User Login info
   const database = [
     {
       username: "admin",
@@ -20,36 +19,29 @@ function App() {
   };
 
   const handleSubmit = (event) => {
-    //Prevent page reload
     event.preventDefault();
 
     var { uname, pass } = document.forms[0];
 
-    // Find user login info
     const userData = database.find((user) => user.username === uname.value);
 
-    // Compare user info
     if (userData) {
       if (userData.password !== pass.value) {
-        // Invalid password
         setErrorMessages({ name: "pass", message: errors.pass });
       } else {
         setIsSubmitted(true);
         window.location.href = '/adminhome';
       }
     } else {
-      // Username not found
       setErrorMessages({ name: "uname", message: errors.uname });
     }
   };
 
-  // Generate JSX code for error message
   const renderErrorMessage = (name) =>
     name === errorMessages.name && (
       <div className="error">{errorMessages.message}</div>
     );
 
-  // JSX code for login form
   const renderForm = (
     <div className="form">
       <form onSubmit={handleSubmit}>

f0988f687ad0c5dbcf4889fd3d91cac7dac56f84
chamod445 <chamodsanjula445@gmail.com>
2022-05-24 22:45:46 +0530

    Updated

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -7,9 +7,9 @@ export default class navbar extends Component {
 
       <div className='navb'>
         <ul className='navbar nav justify-content-center ul'>
-          <li className='li'><a href="/adminhome">Admin Home</a></li>
-          <li className='li'><a href="/memberlist">Member List</a></li>
-          <li className='li'><a href="/workoutplans">Workout Plans</a></li>
+          <li className='li'><a href="/adminhome"><i class="fa-solid fa-user-shield"></i>&nbsp;&nbsp;Admin Home</a></li>
+          <li className='li'><a href="/memberlist"><i class="fa-solid fa-users">&nbsp;&nbsp;</i>Member List</a></li>
+          <li className='li'><a href="/workoutplans"><i class="fa-solid fa-dumbbell">&nbsp;&nbsp;</i>Workout Plans</a></li>
         </ul>
       </div>
 
